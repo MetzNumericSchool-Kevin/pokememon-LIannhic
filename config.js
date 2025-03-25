15ca5afc05bfc9cc46ecb8359b42df65cfbc2d08
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-03-25 09:21:53 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+export const config = {
+    numberOfParticles: 700,
+    maxParticleSize: 60,
+    particleSpeedX: 3,
+    particleSpeedY: 8,
+    particleAlphaDecay: 0.015,
+    particleLife: 2,
+    particleLifeDecay: 0.008,
+    redValue: 255,
+    greenValue: 150,
+    minParticles: 500,
+};
+
+export const TenebreConfig = {
+    darknessIncrement: 0.01,
+    animationIntervalDelay: 50,
+  };
